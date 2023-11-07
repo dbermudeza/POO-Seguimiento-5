ca046f1fa2c3f3a6942dc327ebacedcc6ad76896
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-11-07 14:59:51 -0500

    Subo el ejercicio #2, con interfaces graficas

---
+++ src/main/java/com/mycompany/pootaller5ej2/FiguraGeometrica.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pootaller5ej2;
+
+public class FiguraGeometrica {
+    private double volumen;
+    private double superficie;
+    
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+    
+    public void setSuperficie(double superficie){
+        this.superficie = superficie;
+    }
+    
+    public double getVolumen(){
+        return this.volumen;
+    }
+    
+    public double getSuperficie(){
+        return this.superficie;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/pootaller5ej2/POOtaller5Ej2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pootaller5ej2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class POOtaller5Ej2 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal=new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
